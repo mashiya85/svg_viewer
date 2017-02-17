cdadf30d897b4ff1f639a5360ef82828f0762eb8
Murali Shankar <mshankar@slac.stanford.edu>
2017-02-17 15:36:01 -0800

    Use a relative path to the viewer.

--- archViewer.js
+++ archViewer.js
@@ -47,7 +47,7 @@ viewerVars.timerIndexFor3DAnimation = -1;
 // This should default to a path relative location that works from the appliance UI.
 // To develop/debug, override this to a absolute URL of the server with the data you are going to use for debugging/developing.
 // You can use the serverURL parameter to accomplish this.
-viewerVars.serverURL = "http://localhost:17668/retrieval/data";
+viewerVars.serverURL = "../../retrieval/data";
 
 // Google finance like list of time windows..
 viewerVars.selectorOptions = {
